2787ae0323bbf266cdc1363810ac0705c4ca9665
Sasmita <sasmitapradhan01@gmail.com>
2022-10-16 01:03:52 -0400

    dcc commit

---
+++ src/main/java/dev/danvegaa/dccc/DcccApplication.java
@@ -0,0 +1,13 @@
+package dev.danvegaa.dccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danvegaa/dccc/DcccApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvegaa.dccc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
